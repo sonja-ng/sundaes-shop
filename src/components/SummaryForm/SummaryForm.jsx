76b36c3fc272c166e2ef8577e975f1d5aff1e412
Sonja <sonja.ng1@gmail.com>
2022-05-01 18:22:11 -0400

    Complete SummaryForm tests

--- src/components/SummaryForm/SummaryForm.jsx
+++ src/components/SummaryForm/SummaryForm.jsx
@@ -1,22 +1,31 @@
 import React, { useState } from 'react';
+import './SummaryForm.css'
 
 const SummaryForm = () => {
     const [ disableStatus, setDisableStatus ] = useState(true)
+    const [ displayStatus, setDisplayStatus ] = useState("hidden")
 
     return (
-        <form>
-            <label>I agree to terms and conditions
-                <input 
-                    type="checkbox"
-                    onChange={(e)=> setDisableStatus(!e.target.checked)}
-                />
-            </label>
-            <button 
-                disabled={disableStatus}
-            >
-            Confirm Order
-            </button>
-        </form>
+        <>
+            <form>
+                <label 
+                    onMouseEnter={()=> setDisplayStatus("")}
+                    onMouseLeave={() => setDisplayStatus("hidden")}
+                >
+                I agree to terms and conditions
+                    <input 
+                        type="checkbox"
+                        onChange={(e)=> setDisableStatus(!e.target.checked)}
+                    />
+                </label>
+                <button 
+                    disabled={disableStatus}
+                >
+                Confirm Order
+                </button>
+            </form>
+            <div className={displayStatus}>No ice cream</div>
+        </>
     )
 }
 
--- src/components/SummaryForm/__test__/SummaryForm.test.js
+++ src/components/SummaryForm/__test__/SummaryForm.test.js
@@ -1,6 +1,7 @@
-import SummaryForm from './SummaryForm'
-import { render, fireEvent, screen } from '@testing-library/react'
+import SummaryForm from '../SummaryForm'
+import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+import userEvent from '@testing-library/user-event'
 
 test('initial checkbox should be unchecked', () => {
     render(<SummaryForm />)
@@ -21,13 +22,27 @@ test('if checkbox is checked, button should be enabled. Else, button should be d
     const submitBtn = screen.getByRole('button', { name: /confirm order/i })
     const checkboxEl = screen.getByRole('checkbox', { name: /i agree to terms and conditions/i })
 
-    fireEvent.click(checkboxEl)
+    userEvent.click(checkboxEl)
 
     expect(checkboxEl).toBeChecked()
     expect(submitBtn).toBeEnabled()
 
-    fireEvent.click(checkboxEl)
+    userEvent.click(checkboxEl)
 
     expect(checkboxEl).not.toBeChecked()
     expect(submitBtn).toBeDisabled()
 });
+
+test('Popover responds to hover', () => {
+    render(<SummaryForm/>)
+    const checkboxLabelEl = screen.getByLabelText(/i agree to terms and conditions/i)
+    const popoverEl = screen.getByText(/no ice cream/i) //use query instead of get if you expect it NOT to be in DOM
+
+    expect(popoverEl).toHaveClass('hidden');
+
+    userEvent.hover(checkboxLabelEl)
+    expect(popoverEl).not.toHaveClass('hidden');
+
+    userEvent.unhover(checkboxLabelEl)
+    expect(popoverEl).toHaveClass('hidden')
+})
